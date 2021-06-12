af8e7f829da5f5be5c69851401bee20264ccc678
Aleksandr Rykalin <arykalin@gmail.com>
2021-06-12 14:03:12 +0300

    refactoring

--- src/state/reducers/current_stage.js
+++ src/state/reducers/current_stage.js
@@ -3,7 +3,7 @@ const initialState = {
 };
 
 export const currentStageReducer = (state = initialState, action) => {
-    console.log("action:", action)
+    console.log("currentStageReducer action:", action)
     switch(action.type) {
         case 'SET_CURRENT_STAGE_TITLE': return {
             title: action.title,
